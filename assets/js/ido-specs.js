fa6a102c801254d94d8ab6340c1797dc9682f9a2
Sheetal <sheetal.saraswat@onsumaye.com>
2021-10-04 16:19:51 +0530

    after gulp installation

--- assets/js/ido-specs.js
+++ assets/js/ido-specs.js
@@ -131,11 +131,11 @@ jQuery(document).ready(function(){
 		jQuery('#alertMsg').remove();
 	});
 	jQuery("#contact-button").click(function() {
-        if(jQuery(this).css("left") == "-319px"){
+        if(jQuery(this).css("left") == "319px"){
             jQuery(this).animate({"left": "0px"});
         }
         else{
-            jQuery(this).animate({"left": "-319px"});
+            jQuery(this).animate({"left": "319px"});
          }
         if(jQuery(this).parent().css("left") == "-320px"){
             jQuery(this).parent().animate({"left": "0px"});
