3728c66030ae9babca779fdcb6e8ec5a2a3a6735
Sheetal <sheetal.saraswat@onsumaye.com>
2022-02-17 13:05:00 +0530

    new yml

--- assets/js/main.js
+++ assets/js/main.js
@@ -9653,6 +9653,18 @@ jQuery(window).on('load', function(){
    jQuery('.menuIcon').on('click', function(){
         jQuery(this).parent('.toggleMenu').toggleClass('showMenu')
     })
+    setTimeout(function(){
+		const breadcrumbs    = document.querySelector('#breadcrumbs');
+		const allActiveTabs  = document.querySelectorAll('.openList');
+		let nodeList = '';
+		allActiveTabs.forEach(function(activeTab, i) {
+			nodeList = (i == 0) ? activeTab.firstElementChild.innerHTML : nodeList+" > "+activeTab.firstElementChild.innerHTML;
+			//console.log(nodeList);
+		});
+        const linkText =  jQuery('.sidebar-docs .leftSection .collapsedArea ul li span a[href="'+pathname+'"]').text();
+		breadcrumbs.insertAdjacentHTML("beforeend", nodeList+ " > " +linkText);
+
+	}, 10)
 })
 jQuery(window).on('resize', function(){
     if(jQuery(window).width() >= 960){
@@ -9666,19 +9678,4 @@ jQuery(window).on('scroll', function(){
     else{
         jQuery('.leftSection').removeClass('leftSectionFixed');
     }
-})
-
-// Added by Gaurav kumar
-jQuery(window).on('load', function(){
-	setTimeout(function(){
-		const breadcrumbs    = document.querySelector('#breadcrumbs');
-		const allActiveTabs  = document.querySelectorAll('.openList');
-		let nodeList = '';
-		allActiveTabs.forEach(function(activeTab, i) {
-			nodeList = (i == 0) ? activeTab.firstElementChild.innerHTML : nodeList+" > "+activeTab.firstElementChild.innerHTML;
-			//console.log(nodeList);
-		});
-		breadcrumbs.insertAdjacentHTML("beforeend", nodeList);
-
-	}, 10)
 })
